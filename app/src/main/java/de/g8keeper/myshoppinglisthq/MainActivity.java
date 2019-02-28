61dc625559da2555b7688926ae7b5b856d1543e7
g8keeper <seb.qgler@gmail.com>
2019-02-28 15:55:05 +0100

    Stand 2019028

--- app/src/main/java/de/g8keeper/myshoppinglisthq/MainActivity.java
+++ app/src/main/java/de/g8keeper/myshoppinglisthq/MainActivity.java
@@ -2,8 +2,11 @@ package de.g8keeper.myshoppinglisthq;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -23,22 +26,34 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE = 4711;
 
 
     private ListView mShoppingMemosListView;
     private boolean isKeyboardClick = false;
     ShoppingMemoDataSource dataSource;
+
 
 
     /***********************************************************************************************
@@ -114,7 +129,9 @@ public class MainActivity extends AppCompatActivity {
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
                 TextView textView = (TextView) view;
-
+//                CheckedTextView textView = (CheckedTextView) view;
+//                textView.getCheckMarkDrawable().setVisible(false,false);
+//                textView.;
 
 
                 ShoppingMemo memo = (ShoppingMemo) mShoppingMemosListView.getItemAtPosition(position);
@@ -276,6 +293,13 @@ public class MainActivity extends AppCompatActivity {
                                 AlertDialog editShopingMemoDialog = createShoppingMemoDialog(shoppingMemo);
                                 editShopingMemoDialog.show();
 
+
+
+
+//                                Log.d(TAG, "onActionItemClicked: " +getCurrentFocus().toString());
+//
+//                                Log.d(TAG, "onActionItemClicked: requestFocus: " +etNewQuantity.requestFocus());
+//                                Log.d(TAG, "onActionItemClicked: " +getCurrentFocus().toString());
                                 // zeigt das soft-keyboard an
 
 //                                imm.showSoftInput(getCurrentFocus(),InputMethodManager.SHOW_IMPLICIT);
@@ -353,7 +377,11 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-        etNewQuantity.setSelection(0, etNewQuantity.length());
+
+
+//        etNewQuantity.setShowSoftInputOnFocus(true);
+//        Log.d(TAG, "requestFocus: " +etNewQuantity.requestFocus());
+
 
         return builder.create();
 
@@ -369,5 +397,96 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void startScan(View view){
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+
+        intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (RuntimeException e) {
+            Toast.makeText(this, "Scanner nicht gefunden", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+            TextView etProduct = findViewById(R.id.et_product);
+            etProduct.setText(getProductName(data.getStringExtra("SCAN_RESULT")));
+            TextView etQuantity = findViewById(R.id.et_quantity);
+//            etQuantity.requestFocus();
+            etQuantity.post(()->etQuantity.requestFocus());
+        } else {
+            Toast.makeText(this, "Scan nicht möglich", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private String getProductName(String scanResult) {
+        GetProductDataTask task = new GetProductDataTask();
+        String result = null;
+
+        try {
+            result = task.execute(scanResult).get();
+            JSONObject rootObject = new JSONObject(result);
+            Log.d(TAG, "JSON:");
+            Log.d(TAG, rootObject.toString(2));
+            if(rootObject.has("product")){
+                JSONObject productObject = rootObject.getJSONObject("product");
+                if (productObject.has("product_name")){
+                    return productObject.getString("product_name");
+                }
+            }
+
+        } catch (ExecutionException e) {
+            Log.e(TAG, "ExecutionException", e);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "InterruptedException", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "JSONException", e);
+        }
+
+        return "Artikel nicht gefunden";
+
+    }
+
+
+    public class GetProductDataTask extends AsyncTask<String, Void, String> {
+
+//        private final String TAG = GetProductDataTask.class.getSimpleName();
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            final String baseUrl = "https://world.openfoodfacts.org/api/v0/product/"; //[barcode].json"
+            final String requestUrl = baseUrl + strings[0] + ".json";
+
+            StringBuilder result = new StringBuilder();
+            URL url = null;
+
+            Log.d(TAG, "URL: " + requestUrl);
+
+            try {
+                url = new URL(requestUrl);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "", e);
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            } catch (IOException e) {
+                Log.e(TAG, "", e);
+            }
+
+
+            return result.toString();
+        }
+    }
 
 }
